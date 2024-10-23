958c8c823413b2dc3a5dd43f7947a589e42da456
Felipe Vasquez <132226848+felivasquez@users.noreply.github.com>
2024-10-23 11:29:04 -0300

    Delete biblioTec directory

--- biblioTec/app/config/conex.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servidor = "localhost";
-$usuario = "root";
-$pass = "";
-$base = "Biblioteca";
-
-$conexion = mysqli_connect($servidor, $usuario, $pass, $base) or die("Problemas en la conexión");
-
-?>
